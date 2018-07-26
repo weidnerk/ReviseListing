838d7ff56b49739e1bca606463a31ffa43b64780
Kevin Weidner <weidnerk@gmail.com>
2018-07-26 15:16:02 -0400

    Update to process PostedListings table.

--- reviselisting/Program.cs
+++ reviselisting/Program.cs
@@ -2,8 +2,11 @@
 using eBay.Service.Core.Sdk;
 using eBay.Service.Core.Soap;
 using eBay.Service.Util;
+using reviselisting.Models;
+using sclib;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +15,102 @@ namespace reviselisting
 {
     class Program
     {
+        static DataModelsDB db = new DataModelsDB();
+        private static string Log_File = "out_of_stock.txt";
+
         static void Main(string[] args)
         {
             //ReviseFixedPriceItem("223040988437", 99.95);
 
-            EndFixedPriceItem("223040988359");
-            EndFixedPriceItem("223040988437");
+            //EndFixedPriceItem("223040988359");
+            //EndFixedPriceItem("223040988437");
+
+            Task.Run(async () =>
+            {
+                await ScanListings();
+
+            }).Wait();
+            Console.WriteLine("complete");
+           // Console.ReadKey();
+
+        }
+
+        protected static async Task ScanListings()
+        {
+            string appid = ConfigurationManager.AppSettings["appID"];
+
+            int count = db.PostedListings.Count();
+            int i = 0;
+            //foreach (PostedListing p in db.PostedListings.Where(r => r.ListedItemID == "223074116525"))
+            foreach (PostedListing p in db.PostedListings)
+            {
+                try
+                {
+                    string sourcrUrl = p.SourceUrl;
+                    var result = await Scrape.GetDetail(sourcrUrl);
+                    Console.WriteLine("processing " + (++i) + " of " + count.ToString());
+                    if (!string.IsNullOrEmpty(result.availability))
+                    {
+                        if (result.availability.Contains("Out of stock"))
+                        {
+                            var myListing = await ebayAPIs.GetSingleItem(p.ListedItemID, appid);
+                            if (myListing.Qty > 0)
+                            {
+                                Console.WriteLine("OUT OF STOCK " + p.Title);
+                                string reviseResult = ReviseQty(p.ListedItemID, 0);
+                                dsutil.DSUtil.WriteFile(Log_File, p.ListedItemID + " " + p.Title);
+                                dsutil.DSUtil.WriteFile(Log_File, reviseResult);
+                            }
+                            else
+                            {
+                                dsutil.DSUtil.WriteFile(Log_File, p.ListedItemID + " " + p.Title);
+                                dsutil.DSUtil.WriteFile(Log_File, "Listing quantity already set to 0 - no action taken.");
+                            }
+                            dsutil.DSUtil.WriteFile(Log_File, string.Empty);
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    dsutil.DSUtil.WriteFile(Log_File, "ERROR: " + p.ListedItemID + " " + exc.Message);
+                }
+            }
+            dsutil.DSUtil.WriteFile(Log_File, string.Format("Processed {0} listings.", count));
+        }
+
+        private static string ReviseQty(string itemID, int qty)
+        {
+
+            //create the context
+            ApiContext context = new ApiContext();
+
+            //set the User token
+            string token = AppSettingsHelper.Token;
+            context.ApiCredential.eBayToken = token;
+
+            //set the server url
+            string endpoint = AppSettingsHelper.Endpoint;
+            context.SoapApiServerUrl = endpoint;
+
+            //enable logging
+            context.ApiLogManager = new ApiLogManager();
+            context.ApiLogManager.ApiLoggerList.Add(new FileLogger("ebay_log.txt", true, true, true));
+            context.ApiLogManager.EnableLogging = true;
+
+            //set the version
+            context.Version = "817";
+            context.Site = SiteCodeType.US;
+
+            ReviseFixedPriceItemCall reviseFP = new ReviseFixedPriceItemCall(context);
+
+            ItemType item = new ItemType();
+            item.ItemID = itemID;
+            item.Quantity = qty;
+
+            reviseFP.Item = item;
+
+            reviseFP.Execute();
+            return reviseFP.ApiResponse.Ack.ToString();
         }
 
         private static void ReviseFixedPriceItem(string itemID, double price)
